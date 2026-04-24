4c3c2ff614ff0022a6dd2314185b2738275a3068
Gabriel <gabrielmenatoconsentino@gmail.com>
2026-04-24 18:03:03 -0300

    commit dia2

---
+++ menuh.js
@@ -0,0 +1,10 @@
+function menu() {
+    const menuh = document.getElementById("menuh")
+
+    if (nav.style.display == 'none') {
+        nav.style.display = 'flex'
+    }
+    else {
+    nav.style.display = 'none'
+    }
+}
