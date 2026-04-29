c4f2329906914582cb51e1d62682c8e090761ff2
Gabriel <gabrielmenatoconsentino@gmail.com>
2026-04-29 13:16:52 -0300

    commit terca

---
+++ faq.js
@@ -0,0 +1,5 @@
+function abrirCaixa(caixaP) {
+    const rodape = document.getElementById("caixaP")
+
+    
+}
--- menuh.js
+++ menuh.js
@@ -1,10 +1,23 @@
+const menuh = document.getElementById("menuh")
+const sobrep = document.getElementById("overlay")
+
 function menu() {
-    const menuh = document.getElementById("menuh")
 
     if (nav.style.display == 'none') {
         nav.style.display = 'flex'
+        overlay.style.display = 'flex'
     }
     else {
     nav.style.display = 'none'
+    overlay.style.display = 'none'
     }
+}
+
+function fechaMenu() {
+
+    if (overlay.style.display = 'flex') {
+        nav.style.display = 'none'
+        overlay.style.display = 'none'
+    }
+        
 }
