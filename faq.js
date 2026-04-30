76bb3f679389bf505983200d959889524fb36fe6
Gabriel <gabrielmenatoconsentino@gmail.com>
2026-04-30 18:01:43 -0300

    gabrielquinta

--- faq.js
+++ faq.js
@@ -1,40 +1,52 @@
 function abrirCaixa1() {
     let rodape1 = document.getElementById('caixaP1')
+    let img = document.getElementById('mudarSeta1')
 
     if(rodape1.style.display == 'block') {
        rodape1.style.display = 'none'
+       img.textContent = "\uF181"
     }
     else {
         rodape1.style.display = 'block'
+        img.textContent = "\uF182"
     }   
 }
 function abrirCaixa2() {
     let rodape2 = document.getElementById('caixaP2')
+    let img = document.getElementById('mudarSeta2')
 
     if(rodape2.style.display == 'block') {
        rodape2.style.display = 'none'
+       img.textContent = "\uF181"
     }
     else {
         rodape2.style.display = 'block'
+        img.textContent = "\uF182"
     }   
 }
 function abrirCaixa3() {
     let rodape3 = document.getElementById('caixaP3')
+    let img = document.getElementById('mudarSeta3')
 
     if(rodape3.style.display == 'block') {
        rodape3.style.display = 'none'
+       img.textContent = "\uF181"
     }
     else {
         rodape3.style.display = 'block'
+        img.textContent = "\uF182"
     }   
 }
 function abrirCaixa4() {
     let rodape4 = document.getElementById('caixaP4')
+    let img = document.getElementById('mudarSeta4')
 
     if(rodape4.style.display == 'block') {
        rodape4.style.display = 'none'
+       img.textContent = "\uF181"
     }
     else {
         rodape4.style.display = 'block'
+        img.textContent = "\uF182"
     }   
 }
